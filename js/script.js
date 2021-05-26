b7a6a6cc86c125f8724ee5db0c1737b14b069fed
Dmitry <dpgomberg@gmail.com>
2021-05-26 13:12:36 +0300

    dsgsdg

--- js/script.js
+++ js/script.js
@@ -25,9 +25,35 @@ $(document).ready(function () {
          }
       }
    });
-   $(function(){
-       $('.only-one').on('click',function(){
-          $('.only-one').not(this).prop('checked',false);
-       });
-    });
+   $(function () {
+      $('.only-one').on('click', function () {
+         $('.only-one').not(this).prop('checked', false);
+      });
+   });
+
+   $('.select').on('click', '.select__head', function () {
+      if ($(this).hasClass('open')) {
+         $(this).removeClass('open');
+         $(this).next().fadeOut();
+      } else {
+         $('.select__head').removeClass('open');
+         $('.select__list').fadeOut();
+         $(this).addClass('open');
+         $(this).next().fadeIn();
+      }
+   });
+
+   $('.select').on('click', '.select__item', function () {
+      $('.select__head').removeClass('open');
+      $(this).parent().fadeOut();
+      $(this).parent().prev().text($(this).text());
+      $(this).parent().prev().prev().val($(this).text());
+   });
+
+   $(document).click(function (e) {
+      if (!$(e.target).closest('.select').length) {
+         $('.select__head').removeClass('open');
+         $('.select__list').fadeOut();
+      }
+   });
 });
