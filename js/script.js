0ad596f99b36ec3dd456718158e9cf2267835e50
Dmitry <dpgomberg@gmail.com>
2021-05-26 10:02:13 +0300

    fdgdf

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-    $('.burger-menu, .header__burger').click(function(event) {
-        $('.header__bottom').toggleClass('_active');
-        $('body').toggleClass('lock');
-        console.log('dssd');
-    });
+$(document).ready(function () {
+   $('.burger-menu, .header__burger').click(function (event) {
+      $('.header__bottom').toggleClass('_active');
+      $('body').toggleClass('lock');
+   });
+   const swiper = new Swiper('.swiper-container', {
+      
+      spaceBetween: 30,
+      navigation: {
+         nextEl: '.swiper-next',
+         prevEl: '.swiper-prev',
+      },
+      breakpoints: {
+         // when window width is >= 320px
+         100: {
+            slidesPerView: 1
+         },
+         // when window width is >= 480px
+         750: {
+            slidesPerView:2
+         },
+         // when window width is >= 640px
+         1100: {
+            slidesPerView:3
+         }
+      }
+   });
 });
